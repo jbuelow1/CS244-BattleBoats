98ac681a7c06a2a18177273dda032f1ebed7f687
yammyshep <jdbuelow@gmail.com>
2021-10-03 23:15:04 -0500

    fixed merge conflicts

--- Strike.h
+++ Strike.h
@@ -2,21 +2,12 @@
 
 #include "BoardPiece.h"
 #include "Player.h"
-#include "BattleBoatBoard.h"
-#include "Boat.h"
-
-class Player;
-
-class BattleBoatBoard;
-
-class Boat;
 
 class Strike : public BoardPiece {
 public:
-	Strike(Player &owner, int location[3]);
-	bool computeHit(BattleBoatBoard& board);
+	Strike(Player& owner, int location[2][10][10]);
+	void computeHit(BattleBoatBoard board);
 	bool getIsHit() const;
-    bool wouldHit(Boat& b);
 private:
 	bool isHit;
 };
